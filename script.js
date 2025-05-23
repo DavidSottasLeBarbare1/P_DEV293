e9be314e31da3915a6b27368b24a20f69b441807
David Sottas <pk88sdk@eduvaud.ch>
2025-05-23 16:31:58 +0200

    removed every html for pepetSearch and replaced with js, also added product page

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 var modalProduct = document.getElementById("modalProduct");
-var img = document.getElementById("imgProduct");
-var modalImg = document.getElementById("modal-content");
-var captionText = document.getElementById("caption");
+var modalImg = document.getElementById("modalContent");
 var span = document.getElementsByClassName("close")[0];
+var img = document.getElementById("imgProduct");
+var imgSearchTab = document.getElementsByClassName("imgSearch");
+var titleProduct = document.getElementById("titleProduct");
+for (let i = 0; i < imgSearchTab.length; i++) {
+  var imgSearch = imgSearchTab[i];
 
-img.onclick = function(){
-  modalProduct.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+  imgSearch.onclick = function () {
+    localStorage.setItem('stockAlt', this.alt);
+    localStorage.setItem('stockSrc', this.src);
+    window.location.href = 'product.html';
+  }
 }
 
-span.onclick = function() {
+var altText = localStorage.getItem('stockAlt');
+var imgSrc = localStorage.getItem('stockSrc');
+titleProduct.innerHTML = altText;
+img.alt = altText;
+img.src = imgSrc;
+
+img.onclick = function () {
+  modalProduct.style.display = "block";
+  modalImg.src = this.src;
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      modalProduct.style.display = "none";
+    }
+  })
+}
+  ;
+
+span.onclick = function () {
   modalProduct.style.display = "none";
 }
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
+document.getElementById('contactForm').addEventListener('submit', function (event) {
   event.preventDefault();
   alert('Votre message a été envoyé. Merci de nous avoir contactés!');
   this.reset();
